5057bffe5819f31aea4eb8c734dd8ab036a1e547
Rojan <shrestharojan79@gmail.com>
2024-12-14 20:10:16 +0545

    feature-navbar complete debug-dependencies

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,33 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Navbar from "../src/scenes/navbar";
 import { SelectedPage } from "./shared/types";
 
 function App() {
-  const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
+  const [selectedPage, setSelectedPage] = useState<SelectedPage>(
+    SelectedPage.Home
+  );
+
+  const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+  useEffect(() => {
+    if (window.scrollY === 0) {
+      setIsTopOfPage(true);
+      setSelectedPage(SelectedPage.Home);
+    } else {
+      setIsTopOfPage(false);
+    }
+    //This is required every time we use window functions jastai hamle window.scrollY haru use garem tesma yo kamlagxa
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
   return (
     <div className="app bg-gray-20">
-      <Navbar selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+      <Navbar isTopOfPage={isTopOfPage} selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
     </div>
   );
 }
 
 export default App;
+
+function handleScroll(this: Window, ev: Event) {
+  throw new Error("Function not implemented.");
+}
