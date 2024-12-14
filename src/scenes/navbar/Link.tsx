97873ba6aed18d8064a3133c9d3bdef82ca500a8
Rojan <shrestharojan79@gmail.com>
2024-12-14 18:22:11 +0545

    fix

--- src/scenes/navbar/Link.tsx
+++ src/scenes/navbar/Link.tsx
@@ -1,13 +1,15 @@
 import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "../../shared/types";
 
 type Props = {
   page: string;
-  selectedPage: string;
-  setSelectedPage: (value: string) => void;
+  selectedPage:SelectedPage;
+  setSelectedPage: (value: SelectedPage) => void;
 };
 
 const Link = ({ page, selectedPage, setSelectedPage }: Props) => {
-  const lowerCasePage = page.toLowerCase().replace(/ /g, "");
+  // what as keyword is doing is that it is forcing to change lowecasepage into selectedpage type enum
+  const lowerCasePage = page.toLowerCase().replace(/ /g, "") as SelectedPage;
   return (
     <AnchorLink
       className={`${selectedPage === lowerCasePage ? "text-primary-500" : ""} transition duration-500 hover:text-primary-300`}
