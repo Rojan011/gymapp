e015385b1f7100b6ece2ada1c0b0f8da72015cfb
Rojan <shrestharojan79@gmail.com>
2024-12-16 17:51:25 +0545

    fix-homeImage

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { useEffect, useState } from "react";
 import Navbar from "../src/scenes/navbar";
 import { SelectedPage } from "./shared/types";
-import Home from "./scenes/navbar/home";
+import Home from "./scenes/home";
+import Benefits from "./scenes/benefits";
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(
@@ -32,6 +33,7 @@ function App() {
         setSelectedPage={setSelectedPage}
       />
       <Home setSelectedPage={setSelectedPage} />
+      <Benefits setSelectedPage={setSelectedPage} />
     </div>
   );
 }
